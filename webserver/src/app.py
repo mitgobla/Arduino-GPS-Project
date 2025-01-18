b6e1bda2deb1e0d01159e6082f6823cb143ee8aa
Ben Dodd <ben-dodd@outlook.com>
2025-01-18 23:00:08 +0000

    Database connection

--- webserver/src/app.py
+++ webserver/src/app.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from geoalchemy2 import Geometry
+import json
 
 app = Flask(__name__)
-app.config.from_object("config.Config")
+app.config.from_object("src.config.Config")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+class DeviceData(db.Model):
+    __tablename__ = "device_data"
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(Geometry(geometry_type="POINT", srid=4326))
+    # timestamp with timezone
+    recorded = db.Column(db.DateTime(timezone=True))
+    altitude = db.Column(db.Float)
+    temperature = db.Column(db.Float)
+    humidity = db.Column(db.Float)
+    device = db.Column(db.String(255))
+    # timestamp no timezone
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+
 @app.route("/")
 def index():
-    return "Hello World!"
+    query = DeviceData.query.count()
+    return f"Hello World! There are {query} records in the database."
--- webserver/src/config.py
+++ webserver/src/config.py
@@ -1,8 +1,15 @@
 import dotenv
 import os
+import urllib.parse
 
 dotenv.load_dotenv()
 
 class Config:
     DEBUG = bool(os.getenv('DEBUG', False))
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    DATABASE_USERNAME = os.getenv('DATABASE_USERNAME', "")
+    DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', "")
+    DATABASE_HOSTNAME = os.getenv('DATABASE_HOSTNAME', "localhost")
+    DATABASE_PORT = os.getenv('DATABASE_PORT', "5432")
+    DATABASE_NAME = os.getenv('DATABASE_NAME', "postgres")
+    # Use urllib.parse.quote_plus to escape special characters in the password
+    SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{DATABASE_USERNAME}:{urllib.parse.quote_plus(DATABASE_PASSWORD)}@{DATABASE_HOSTNAME}:{DATABASE_PORT}/{DATABASE_NAME}"
